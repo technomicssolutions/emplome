1fe68d5a216572b8d1249eb65ec2eb260006437d
anuj <anuj@technomicssolutions.com>
2014-03-20 14:38:42 +0530

    changes added

--- emplome/web/urls.py
+++ emplome/web/urls.py
@@ -5,6 +5,7 @@ from web.views import *
 urlpatterns = patterns('',
 	url(r'^$', Home.as_view(), name='home'),
 	url(r'^full-time/', FullTime.as_view(), name='full_time'),
+	url(r'^recruiter/', RecruiterHomeView.as_view(), name='recruiter'),
 	url(r'^recruiter-reg/', RecruiterRegistrationView.as_view(), name='register'),
 	url(r'^job_seeker_registration$', JobSeekerRegistration.as_view(), name='job_seeker_registration'),
 	url(r'^full-time/', FullTime.as_view(), name='full_time')
